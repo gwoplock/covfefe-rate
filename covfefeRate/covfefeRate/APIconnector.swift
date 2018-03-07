204d07a009de4c8d07df2611ae955783fdce590c
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-03-07 16:00:52 -0600

    renamed some UI elements and hooked them to the view controller class also starting an API connector class

---
+++ covfefeRate/covfefeRate/APIconnector.swift
@@ -0,0 +1,13 @@
+//
+//  APIconnector.swift
+//  covfefeRate
+//
+//  Created by Garrett Battaglia on 3/7/18.
+//  Copyright © 2018 Garrett Battaglia. All rights reserved.
+//
+
+import UIKit
+
+class APIconnector: NSObject {
+    
+}
